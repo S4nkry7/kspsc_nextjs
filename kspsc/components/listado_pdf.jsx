f95f4a19ec4c31e3a4d203475393628a64e5a9f6
S4nkry7 <107291299+S4nkry7@users.noreply.github.com>
2025-03-20 14:28:01 -0600

    Se sube configuracion firebase

--- kspsc/components/listado_pdf.jsx
+++ kspsc/components/listado_pdf.jsx
@@ -6,42 +6,53 @@ const ComunicadosWidget = ({ title = "", subtitle = "", tagline = "", id, isDark
   const [activeTab, setActiveTab] = useState(0);
 
   useEffect(() => {
+    // Cargar los comunicados
     const fetchComunicados = async () => {
       const data = await obtenerComunicados();
       setItems(data);
     };
     fetchComunicados();
+
+    // Cargar el script externo de Animación
+    const script = document.createElement("script");
+    script.src = "/src/utils/AnimacionTabs.js"; // Asegúrate de que la ruta sea correcta
+    script.async = true;
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script); // Limpiar el script al desmontar el componente
+    };
   }, []);
 
   return (
     <div>
-            <div data-aos="fade-up" className="mb-4 border-b border-gray-200 dark:border-gray-700 overflow-x-auto">
-                <ul className="flex p-1 rounded-xl bg-blue-gray-50/60 whitespace-nowrap" role="list">
-                {items.map(({ titulo }, index) => (
-                    <li key={titulo} className="z-30 flex-auto text-center">
-                    <button
-                        className={`z-30 flex items-center justify-center w-full px-0 py-1 mb-0 transition-all ease-in-out border-0 rounded-lg cursor-pointer text-slate-50 bg-inherit hover:text-blue-500 hover:border-blue-500 dark:hover:text-blue-500 min-w-[8.33%] ${activeTab === index ? "text-blue-500" : ""}`}
-                        onClick={() => setActiveTab(index)}
-                        aria-controls={`t${titulo}`}
-                    >
-                        <span className="ml-1">{titulo}</span>
-                    </button>
-                    </li>
-                ))}
-                </ul>
-            </div>
+      <div data-aos="fade-up" className="mb-4 border-b border-gray-200 dark:border-gray-700 overflow-x-auto">
+        <ul className="flex p-1 rounded-xl bg-blue-gray-50/60 whitespace-nowrap" role="list">
+          {items.map(({ titulo }, index) => (
+            <li key={titulo} className="z-30 flex-auto text-center">
+              <button
+                className={`z-30 flex items-center justify-center w-full px-0 py-1 mb-0 transition-all ease-in-out border-0 rounded-lg cursor-pointer text-slate-50 bg-inherit hover:text-blue-500 hover:border-blue-500 dark:hover:text-blue-500 min-w-[8.33%] ${
+                  activeTab === index ? "text-blue-500" : ""
+                }`}
+                onClick={() => setActiveTab(index)}
+                aria-controls={`t${titulo}`}
+              >
+                <span className="ml-1">{titulo}</span>
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
       <div className="p-5" data-aos="fade-up">
         {items.map(({ titulo, descripcion }, index) => (
           <div
             key={titulo}
-            className={`$ {
+            className={`${
               activeTab === index ? "block opacity-100" : "hidden opacity-0"
             } rounded-lg shadow-[0_4px_30px_rgba(0,0,0,0.1)] dark:shadow-[0_4px_30px_rgba(0,0,0,0.1)] backdrop-blur border border-[#ffffff29] bg-white dark:bg-slate-900 p-6`}
             id={`t${titulo}`}
           >
-            <h3 className="text-lg font-bold text-gray-900 dark:text-white mb-2">
-              Comunicados año {titulo}:
-            </h3>
+            <h3 className="text-lg font-bold text-gray-900 dark:text-white mb-2">Comunicados año {titulo}:</h3>
             <nav className="flex min-w-[240px] flex-col gap-1 p-2 font-sans text-base font-normal text-blue-gray-700">
               {descripcion.map(({ nombre, url }) => (
                 <li key={nombre} className="list-disc">
